fb30183c506ba3cf489eeea23403587fee048351
unknownstToThem <40240727+unknownstToThem@users.noreply.github.com>
2018-06-25 14:15:12 +0100

    Complete
    Complete

--- tests/json_save_test.py
+++ tests/json_save_test.py
@@ -4,15 +4,14 @@ import os
 
 fname = "test.json"
 log = open("log.txt", "w")
+runs = 5
 
 class Test(object):
 	def __init__(self, a, b, c):
 		self.a = a
 		self.b = b
 		self.c = c
-		print("Created:", a, file=log)
-		print("Created:", b, file=log)
-		print("Created:", c, "\n", file=log)
+		log.write("\nCreated: %d\nCreated: %d\nCreated: %d\n\n" % (a,b,c))
 
 		self.data = {
 		    "save":{
@@ -31,37 +30,45 @@ class Test(object):
 
 		with open(fname, "w") as f: # write save to file
 			f.write(json.dumps(self.data, sort_keys=True, indent=4))
-			print("Saved JSON!", file=log)
+			log.write("Saved JSON!\n")
 
 	def load(self):
 		with open(fname, "r") as f: # open save file
 			json_data = f.read() # get save file contents
 			self.data = json.loads(json_data)
-			self.json = json.dumps(json_data, sort_keys=True, indent=4)
+			self.json = json.dumps(json_data, sort_keys=True, indent=4).replace("\"{", "{").replace("}\"", "}").replace("\\n", "\n").replace("\\", "")
 
 			self.a, self.b, self.c = self.data["save"].values()
 
-			print("Loaded JSON!\n", file=log)
+			log.write("Loaded JSON!\n\n")
 			try:
 				log.write("JSON: %s\n" % self.json) # check if json was successfully converted
-				print("Params: {}, {}, {}\nData: ".format(self.a, self.b, self.c), end="", file=log) # check if parameters were saved & loaded
-				print(*self.data["save"].values(), sep=", ", file=log) # check if data matches parameters
-				print(file=log)
-			except Exception as e:
-				print(sys.exc_info(), file=log)
+				log.write("Params: {}, {}, {}\nData: ".format(self.a, self.b, self.c)) # check if parameters were saved & loaded
+				log.write("%d, %d, %d" % tuple(self.data["save"].values())) # check if data matches parameters
+				log.write("\n")
+			except Exception:
+				error = sys.exc_info()
+				log.write("%s: %s" % (error[0], error[1]))
 
 			del json_data # garbage collection
 			del self.json
 
-print("Run #1\n", file=log)
-temp = Test(2,8,5)
-temp.save()
-temp.load()
+for i in range(runs):
+	log_str = "Run #%d\n\n" % i # save time + easier to write
 
-print("\nRun #2\n", file=log)
-temp.a, temp.b, temp.c = [temp.a*2,temp.b*2,temp.c*2]
-temp.save()
-temp.load()
+	if i is 0: # can do this for low numbers (up to 8-bit numbers, max 255)
+		log.write(log_str)
+		temp = Test(2,8,5) # add values to save
+		temp.save() # save data
+		temp.load() # load data to check data saved
+	else:
+		log_str = "\n\n" + log_str
+
+		log.write(log_str)
+		temp.a, temp.b, temp.c = [x*10 for x in temp.data["save"].values()]
+		temp.save()
+		temp.load()
+
 log.close()
 os.startfile(fname)
 os.startfile("log.txt")
